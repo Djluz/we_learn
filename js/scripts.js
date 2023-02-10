6404374762d0d9d161359a6958dd457e33f07c05
russelj91 <russelj91@gmail.com>
2023-02-11 07:00:41 +0800

    modified:   css/style.css 	modified:   index.html 	modified:   js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
+var lastScrollTop;
+navbar = document.getElementById('navbarbg');
+window.addEventListener('scroll',function(){
+var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+if(scrollTop > lastScrollTop){
+navbar.style.top='-80px';
+}
+else{
+navbar.style.top='0';
+
+}
+lastScrollTop = scrollTop;
+});
 
 var welcome = document.getElementById('section');
 
