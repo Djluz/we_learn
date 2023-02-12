686e0e9559a440e4210ee7fccce35f0c21a88dc7
russelj91 <russelj91@gmail.com>
2023-02-12 21:49:47 +0800

    Master

--- js/addToCart.js
+++ js/addToCart.js
@@ -1,3 +1,4 @@
+
 let cart = JSON.parse(localStorage.getItem('cart')) || [];
 let badge = document.querySelector('#badge');
 let tableBody = document.querySelector('#cart-table-body');
@@ -30,6 +31,8 @@ addToCartButtons.forEach(function(button) {
     });
 });
 
+
+
 function displayCartItems() {
     cart.forEach(function(item) {
       let row = `
@@ -45,30 +48,37 @@ function displayCartItems() {
     });
 }
 
+
 function updateBadge() {
     badge.textContent = cart.length;
 }
 
+
+
+
 tableBody.addEventListener('click', function(event) {
+
+
   if (event.target.classList.contains('delete-from-cart')) {
       let row = event.target.closest('tr');
       let title = row.querySelector('td:nth-child(2)').textContent;
-    
+
       cart = cart.filter(function(item) {
         return item.title !== title;
       });
-    
+
       localStorage.setItem('cart', JSON.stringify(cart));
-    
+
       row.remove();
-    
+
       updateBadge();
   }
 });
 
 
-
 window.addEventListener("beforeunload", function () {
+
+ 
     localStorage.setItem("cart", JSON.stringify(cart));
 });
 
@@ -76,21 +86,28 @@ window.addEventListener("beforeunload", function () {
   
 
 
-  
+ 
 
 function displayCartItems() {
+
+
+
   let total = cart.reduce((sum, item) => sum + Number(item.price.replace(/[^0-9.-]+/g,"")), 0);
+
   let formatter = new Intl.NumberFormat('en-PH', {
     style: 'currency',
     currency: 'PHP'
   });
   
+ 
   let tableFoot = document.querySelector('#cart-total');
   if (tableFoot) {
     tableFoot.textContent = `Total: ${formatter.format(total)}`;
   }
 
   cart.forEach(function(item, index) {
+
+ 
     let row = `
       <tr class="text-center">
         <td><img src="${item.image}" alt="" class="d-none d-sm-block d-sm-none d-md-block d-md-none d-lg-block" style="width: 150px; height: 90px;"></td>
@@ -101,35 +118,47 @@ function displayCartItems() {
       </tr>
     `;
 
+
+
     tableBody.insertAdjacentHTML('beforeend', row);
   });
 }
 
 
 
-
   
   displayCartItems();
-  
-
 
 tableBody.addEventListener('click', function(e) {
   if (!e.target.classList.contains('delete-item')) return;
 
+
+ 
   let index = e.target.dataset.index;
+
   cart.splice(index, 1);
   localStorage.setItem('cart', JSON.stringify(cart));
-
   tableBody.innerHTML = '';
   displayCartItems();
   updateBadge();
 });
 
+
+
 window.addEventListener("storage", function (e) {
+
+ 
   if (e.key === 'cart') {
+
+
     cart = JSON.parse(e.newValue);
+
     tableBody.innerHTML = '';
+
+
     displayCartItems();
+
+
     updateBadge();
   }
 });
@@ -179,24 +208,6 @@ document.getElementById("submit-button").addEventListener("click", function() {
   
 
 
-
-
-
-
-  document.getElementById("submit-button").addEventListener("click", function() {
-    var itemImage = document.getElementById("item-image").value;
-    var itemTitle = document.getElementById("item-title").value;
-    var itemDescription = document.getElementById("item-description").value;
-  
-    $.ajax({
-      type: "POST",
-      url: "save_cart_item.php",
-      data: { item_image: itemImage, item_title: itemTitle, item_description: itemDescription }
-    })
-    .done(function( msg ) {
-      alert( "Data Saved: " + msg );
-    });
-  });
 
 
 
