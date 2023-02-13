95a895c270e6e825a97303862f59affb70652352
Djluz <daryljaneluz@gmail.com>
2023-02-13 12:28:31 +0800

    modified:   css/style.css 	modified:   index.html 	modified:   js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -96,4 +96,22 @@ var swiper = new Swiper(".slide-content", {
         },
     },
   });
+
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "password") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
   
